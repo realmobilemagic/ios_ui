d03083bd0b3f7996b294a2c393c5375a31249b0b
Evandro <evandro.hoffmann@gmail.com>
2022-07-25 18:00:54 -0300

    [CHORE] Added missing content

--- Sources/MobileMagicUI/Extensions/DataExtensions.swift
+++ Sources/MobileMagicUI/Extensions/DataExtensions.swift
@@ -21,3 +21,33 @@ public extension Data {
         return try? Data(contentsOf: path)
     }
 }
+
+import class Foundation.Bundle
+
+private class BundleFinder {}
+
+extension Foundation.Bundle {
+    /// Returns the resource bundle associated with the current Swift module.
+    static var module: Bundle = {
+        let bundleName = "MobileMagicUI_MobileMagicUI"
+
+        let candidates = [
+            // Bundle should be present here when the package is linked into an App.
+            Bundle.main.resourceURL,
+
+            // Bundle should be present here when the package is linked into a framework.
+            Bundle(for: BundleFinder.self).resourceURL,
+
+            // For command-line tools.
+            Bundle.main.bundleURL,
+        ]
+
+        for candidate in candidates {
+            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
+            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
+                return bundle
+            }
+        }
+        fatalError("unable to find bundle named MobileMagicUI_MobileMagicUI")
+    }()
+}
